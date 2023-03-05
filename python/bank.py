8c822fe1c983ae3552c54e706bfac637ad9620ea
VitorMartinsAlves <vitormartinsalves99@gmail.com>
2023-03-05 09:00:07 -0300

    improves

--- python/bank.py
+++ python/bank.py
@@ -1,10 +1,11 @@
+import datetime
 import os
 
 # tentando usar esta função para criar um "pseudo loop"
 
 
 def initFunc(storage, hands):
-    os.system("cls")
+    # os.system("cls")
     print("Seu saldo é de R${:.2f}, você tem R${:.2f} em mãos.\n".format(
         storage, hands))
     choice = int(input("1 - Depósitar\n2 - Sacar\n3 - Transferir\nEscolha:"))
@@ -14,7 +15,8 @@ def initFunc(storage, hands):
 
 
 def switchCase(opt, storage, hands):
-    os.system("cls")
+    # os.system("cls")
+    operator = []
     if opt == 1:
         x = float(input("Valor para ser depósitado R$"))
         if x <= hands:
@@ -25,15 +27,25 @@ def switchCase(opt, storage, hands):
         if b <= storage:
             hands = hands + b
             storage = storage - b
+    if opt == 3:
+        for x in saveChanges:
+            print(x)
     if opt == 0:
         return 0
-
+    # procurar uma maneira melhor de armazenar as transações
+    saveChanges.append("data: "+str(now)+"- Saldo: "+str(storage))
     return initFunc(storage, hands)
 
 
 account = 0
 storage = 0
 hands = 50
+saveChanges = []
+
+now = datetime.datetime.now()
+print(now)
+
+
 initFunc(storage, hands)
 
 # to do
