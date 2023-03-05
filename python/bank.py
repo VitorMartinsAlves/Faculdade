3bc6d351fe9a1a70179685234687f2bae4a5586a
VitorMartinsAlves <vitormartinsalves99@gmail.com>
2023-03-05 07:03:10 -0300

    update

--- python/bank.py
+++ python/bank.py
@@ -1,8 +1,6 @@
 import os
 
-# using this function to make a "pseudo loop"
-
-
+# tentando usar esta função para criar um "pseudo loop"
 def initFunc(storage, hands):
     os.system("cls")
     print("Seu saldo é de R${:.2f}, você tem R${:.2f} em mãos.\n".format(
@@ -10,28 +8,25 @@ def initFunc(storage, hands):
     choice = int(input("1 - Depósitar\n2 - Sacar\n3 - Transferir\nEscolha:"))
     return switchCase(choice, storage, hands)
 
-# trying to make switch case on python
-
-
+# tentando elaborar um switch case em python
 def switchCase(opt, storage, hands):
+    os.system("cls")
     if opt == 1:
-        os.system("cls")
         x = float(input("Valor para ser depósitado R$"))
         if x > hands:
             return initFunc(storage, hands)
         hands = hands - x
         storage = storage + x
-        return initFunc(storage, hands)
     if opt == 2:
-        os.system("cls")
         x = float(input("Valor para ser sacado R$"))
         if x > storage:
             return initFunc(storage, hands)
         hands = hands + x
         storage = storage - x
-        return initFunc(storage, hands)
     if opt == 0:
         return 0
+
+    return initFunc(storage, hands)
 
 
 account = 0
