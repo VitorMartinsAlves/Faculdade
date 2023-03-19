390069155bff3a54832b326ebe6dd044d91fd0d3
VitorMartinsAlves <vitormartinsalves99@gmail.com>
2023-03-19 04:58:03 -0300

    change math detail

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -2,7 +2,7 @@ init = 1
 init_cached = 0
 while 1 == 1:
     print(init)
-    init = init + init_cached
+    init += init_cached
     init_cached = init - init_cached
-    if init > 1000:
+    if init > 99999999:
         break
