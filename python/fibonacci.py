3dc22d32048bb8fbae5338c157339e37d071235e
VitorMartinsAlves <vitormartinsalves99@gmail.com>
2023-03-19 08:14:50 -0300

    fix while and break

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,8 +1,7 @@
 init = 1
 init_cached = 0
-while 1 == 1:
+while init <= 1597:
     print(init)
     init += init_cached
     init_cached = init - init_cached
-    if init > 99999999:
-        break
+
