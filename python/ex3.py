b48ad0ea02a59c8b888272003e453f42251c0ce0
Vitor Martins <vitormartinsalves99@gmail.com>
2023-04-06 23:46:06 +0000

    state

--- python/ex3.py
+++ python/ex3.py
@@ -1,9 +1,9 @@
-data = input("Digite um número")
+data = input("Digite seu genero:")
 
 if data == "F":
-    print(f"Genero masculino")
+    print(f"Genero Feminino")
 else:
     if data == "M":
-        print(f"Genero feminino ")
+        print(f"Genero Masculino")
     else:
         print(f"Você não digitou nem M ou F")
---
+++ python/ex4.py
@@ -0,0 +1,26 @@
+# ex 1
+data = int(input("Digite um número:"))
+if data > 10:
+    print(f"O valor digitado é maior que 10")
+else:
+    if data == 10:
+        print(f"O valor digitado é igual a 10")
+    else:
+        print(f"O valor digitado é menor a 10")
+# ex 2
+x = float(input("Digite seu sálario"))
+y = float(input("Digite seus gastos"))
+# ex 3
+if y > x:
+    print(f"Estourou o orçamento")
+else:
+    print("Gastos dentro do orçamento")
+# ex 4
+a = float(input("Digite um numero"))
+b = float(input("Digite outro numero"))
+
+if a == b:
+    print("Estes numeros são iguais")
+else:
+    print("Estes números são iguais")
+
