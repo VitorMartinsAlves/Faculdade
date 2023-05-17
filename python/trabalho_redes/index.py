03e8ad8553b8fae2be9e95065d22f03c1f771456
Vitor Martins <vitormartinsalves99@gmail.com>
2023-05-17 21:19:30 +0000

    update 2

--- python/trabalho_redes/index.py
+++ python/trabalho_redes/index.py
@@ -2,14 +2,28 @@ import time
 
 
 def groupInfo():
-    print("- Análise e Desenvolvimento de Sistemas\n- Vitor Martins (34333193), Tiago Esteves(7233880930), Ueslei, Alexandre César da silva(32846134)\n- Redes de Computadores e Programação para internet\n- 0.1")
+    print("\n\n- Análise e Desenvolvimento de Sistemas\n- Vitor Martins (34333193), Tiago Esteves(7233880930), Ueslei, Alexandre César da silva(32846134)\n- Redes de Computadores e Programação para internet\n- 0.1\n\n")
     time.sleep(10)
 # print(groupInfo())
 
 
-activeMenu = True
-while activeMenu:
-    print("Menu:\n1- Dados do grupo e aplicação\n2- Resolução da atividade proposta.")
-    choice = int(input("Digite uma opção: "))
-    if choice == 1:
-        groupInfo()
+def decimalConvert(value):
+    converted_value = list(value)
+    acumu = 0
+    for x, index in enumerate(converted_value):
+        print((int(x)**2)+int(index))
+
+    print(acumu)
+
+
+print(1*0)
+# activeMenu = True
+# while activeMenu:
+#     print("\n\n1\nMenu:\n1- Dados do grupo e aplicação\n2- Resolução da atividade proposta.")
+#     choice = int(input("Digite uma opção: "))
+#     if choice == 1:
+#         groupInfo()
+#     if choice == 2:
+#         binarioValue = input(
+#             "Digite o valor que quer converter para decimal: ")
+#         decimalConvert(binarioValue)
