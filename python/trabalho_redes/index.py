1d60a0b6d5791c7da96357ca9541bb28e25c0f92
Vitor Martins <vitormartinsalves99@gmail.com>
2023-05-17 21:26:22 +0000

    finalizado

--- python/trabalho_redes/index.py
+++ python/trabalho_redes/index.py
@@ -1,29 +1,25 @@
 import time
-
-
 def groupInfo():
-    print("\n\n- Análise e Desenvolvimento de Sistemas\n- Vitor Martins (34333193), Tiago Esteves(7233880930), Ueslei, Alexandre César da silva(32846134)\n- Redes de Computadores e Programação para internet\n- 0.1\n\n")
-    time.sleep(10)
-# print(groupInfo())
+    return "\n\n- Análise e Desenvolvimento de Sistemas\n- Vitor Martins (34333193), Tiago Esteves(7233880930), Ueslei (33459878), Alexandre César da silva(32846134)\n- Redes de Computadores e Programação para internet\n- 0.1\n\n"
 
+def convert_bi(binario):
+    decimal = 0
+    potencia = 0
+    while binario != 0:
+        digito = binario % 10
+        decimal += digito * (2 ** potencia)
+        binario //= 10
+        potencia += 1
+    return decimal
 
-def decimalConvert(value):
-    converted_value = list(value)
-    acumu = 0
-    for x, index in enumerate(converted_value):
-        print((int(x)**2)+int(index))
-
-    print(acumu)
-
-
-print(1*0)
-# activeMenu = True
-# while activeMenu:
-#     print("\n\n1\nMenu:\n1- Dados do grupo e aplicação\n2- Resolução da atividade proposta.")
-#     choice = int(input("Digite uma opção: "))
-#     if choice == 1:
-#         groupInfo()
-#     if choice == 2:
-#         binarioValue = input(
-#             "Digite o valor que quer converter para decimal: ")
-#         decimalConvert(binarioValue)
+activeMenu = True
+while activeMenu:
+    print("\n\n\nMenu:\n1- Dados do grupo e aplicação\n2- Resolução da atividade proposta.")
+    choice = int(input("Digite uma opção: "))
+    if choice == 1:
+        print(groupInfo())
+        time.sleep(10)
+    if choice == 2:
+        converted = convert_bi(int(input("Digite um valor para ser convertido para decimal: ")))
+        print(f"O valor convetido é : {converted}")
+        time.sleep(10)
