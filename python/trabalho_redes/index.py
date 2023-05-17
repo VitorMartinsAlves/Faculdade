97e9d59132003cb2df2fe9eb0d9fdc54673a6af9
Vitor Martins <vitormartinsalves99@gmail.com>
2023-05-17 21:38:00 +0000

    algumas modificações finais

--- python/trabalho_redes/index.py
+++ python/trabalho_redes/index.py
@@ -1,6 +1,9 @@
 import time
+
+
 def groupInfo():
     return "\n\n- Análise e Desenvolvimento de Sistemas\n- Vitor Martins (34333193), Tiago Esteves(7233880930), Ueslei (33459878), Alexandre César da silva(32846134)\n- Redes de Computadores e Programação para internet\n- 0.1\n\n"
+
 
 def convert_bi(binario):
     decimal = 0
@@ -13,6 +16,7 @@ def convert_bi(binario):
         # print(digito,decimal,binario,potencia)
     return decimal
 
+
 activeMenu = True
 while activeMenu:
     print("\n\n\nMenu:\n1- Dados do grupo e aplicação\n2- Resolução da atividade proposta.")
@@ -21,6 +25,8 @@ while activeMenu:
         print(groupInfo())
         time.sleep(10)
     if choice == 2:
-        converted = convert_bi(int(input("Digite um valor para ser convertido para decimal: ")))
-        print(f"O valor convetido é : {converted}")
+        orignial_value = int(
+            input("Digite um valor para ser convertido para decimal: "))
+        print(
+            f"O valor de {orignial_value} convetido para base 10 é: {convert_bi(orignial_value)}")
         time.sleep(10)
