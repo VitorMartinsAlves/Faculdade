6cb530115898797059db1d082a11a7e8ff903726
Vitor Martins <54907067+VitorMartinsAlves@users.noreply.github.com>
2023-05-17 20:59:38 +0000

    insert content about group

--- python/trabalho_redes/index.py
+++ python/trabalho_redes/index.py
@@ -1,9 +1,15 @@
-def groupInfo():
-    return "Turma: 1i\nIntegrantes do grupo:\n- Vitor Martins Alves\n- Tiago Esteves Albano (7233880930)\n- Ueslei "
+import time
 
-print(groupInfo())
+
+def groupInfo():
+    print("- Análise e Desenvolvimento de Sistemas\n- Vitor Martins (34333193), Tiago Esteves(7233880930), Ueslei, Alexandre César da silva(32846134)\n- Redes de Computadores e Programação para internet\n- 0.1")
+    time.sleep(10)
+# print(groupInfo())
+
 
 activeMenu = True
 while activeMenu:
-    print("Menu:\n1- Dados do grupo\n2- Resolução da atividade proposta  ")
+    print("Menu:\n1- Dados do grupo e aplicação\n2- Resolução da atividade proposta.")
     choice = int(input("Digite uma opção: "))
+    if choice == 1:
+        groupInfo()
